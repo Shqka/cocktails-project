55ba87dad7da0b669d810fc48adb51ff1b6ea29e
Shqka <hbocq3@gmail.com>
2020-04-25 15:15:00 +0200

    Project Cocktails / Affichage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cocktail } from './cocktail';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cocktails';
+
+  cocktail = new Cocktail('Mojito', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Mojito_003.jpg/220px-Mojito_003.jpg',
+  'Le mojito, prononcé [moˈxito] en espagnol, ou mojito, morito, ou mohito en français, est un cocktail traditionnel de la cuisine cubaine et de la culture de Cuba, à base de rhum, de soda, de citron vert, et de feuilles de menthe fraîche. Inspiré du mint julep, et variante des Ti-punch des Antilles, Daïquiri, et Cuba libre, il est né à Cuba dans les Caraïbes dans les années 1910 (dont il est à ce jour un emblème exotique international).');
 }
---
+++ src/app/cocktails-list/cocktails-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Cocktail } from '../cocktail';
+
+@Component({
+  selector: 'app-cocktails-list',
+  templateUrl: './cocktails-list.component.html',
+  styleUrls: ['./cocktails-list.component.css']
+})
+export class CocktailsListComponent implements OnInit {
+
+  public cocktails: Cocktail[] = [
+    new Cocktail('Mojito', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Mojito_003.jpg/220px-Mojito_003.jpg',
+     'Le mojito, prononcé [moˈxito] en espagnol, ou mojito, morito, ou mohito en français, est un cocktail traditionnel de la cuisine cubaine et de la culture de Cuba, à base de rhum, de soda, de citron vert, et de feuilles de menthe fraîche. Inspiré du mint julep, et variante des Ti-punch des Antilles, Daïquiri, et Cuba libre, il est né à Cuba dans les Caraïbes dans les années 1910 (dont il est à ce jour un emblème exotique international).'),
+    new Cocktail('Margarita',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Blended_Margarita.jpg/170px-Blended_Margarita.jpg',
+    'La Margarita est un cocktail à base de tequila, inventé par des Américains au Mexique. C\'est un before lunch qui serait une version du cocktail daisy (qui signifie « marguerite » en français, « margarita » en espagnol) dans lequel on remplaça le brandy par de la téquila1 durant la prohibition, période où les Américains ouvrirent des bars au Mexique et au Canada dans les zones frontalières.'),
+    new Cocktail('Sour',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Whiskey_sour.jpg/220px-Whiskey_sour.jpg',
+    'Un aigre est une famille traditionnelle de boissons mélangées. Des exemples courants de sours sont la margarita et le side-car. Les sours appartiennent à l\'une des anciennes familles de cocktails originaux et sont décrits par Jerry Thomas dans son livre de 1862, How to Mix Drinks.')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
